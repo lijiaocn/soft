9b286193fc324dc6c0da42731768afb6abfe62fb
lijiaocn <lijiaocn@foxmail.com>
2018-12-02 23:11:41 +0800

    first

--- SUMMARY.md
+++
@@ -1,4 +0,0 @@
-# Summary
-
-* [关于这本书](README.md)
-* [Linux性能优化](chapter1/README.md)
--- run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# run.sh
-# Copyright (C) 2018 lijiaocn <lijiaocn@foxmail.com>
-#
-# Distributed under terms of the GPL license.
-#
-
-nohup gitbook serve --port 4004 2>&1 >/tmp/handbook-linux.log &
