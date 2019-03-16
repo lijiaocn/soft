455f3f81bb6d3f9b70119bac99313406aa16feee
lijiaocn <lijiaocn@foxmail.com>
2019-03-16 15:52:11 +0800

    m

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 # Distributed under terms of the GPL license.
 #
 
-nohup gitbook serve --port 4004 --lrport 35734 2>&1 >/tmp/handbook-linux.log &
-#gitbook serve --port 4004 --lrport 35734 2>&1
+nohup gitbook serve --port 4001 --lrport 35731 2>&1 >/tmp/handbook-linux.log &
+#gitbook serve --port 4001 --lrport 35731 2>&1
